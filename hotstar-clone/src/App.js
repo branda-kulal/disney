d1050cd1a9c6df7288894ee9dda530a86840ed64
branda-kulal <brindakulal97@gmail.com>
2021-01-19 00:07:13 +0530

    state change add

--- hotstar-clone/src/App.js
+++ hotstar-clone/src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
+import { auth } from "./firebase";
 
 function App() {
+  useEffect(() => {
+    //will only run once when the app component loads..
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>>", authUser);
+
+      if (authUser) {
+        //the user just logged in / the user was logged in
+      } else {
+        //the user is logged out
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <div className="app">
